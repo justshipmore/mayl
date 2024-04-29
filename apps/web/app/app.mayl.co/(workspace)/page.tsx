210bb056e77f9133d44e79bd075c16f0adb80698
justshipmore <justshipmore@gmail.com>
2024-04-29 18:20:51 +0100

    Fix broken util package import

--- apps/web/app/app.mayl.co/(workspace)/page.tsx
+++ apps/web/app/app.mayl.co/(workspace)/page.tsx
@@ -1,8 +1,12 @@
+import { APP_WORKSPACE_PATH } from "@repo/utils/constants";
+
 export default function () {
   return (
     <>
       <h3>My workspace</h3>
       <p>You don't have any emails created yet!</p>
+
+      <button>Create new email {APP_WORKSPACE_PATH}</button>
     </>
   );
 }
--- packages/utils/src/constants/index.ts
+++ packages/utils/src/constants/index.ts
@@ -1,2 +1,2 @@
-// path names to be used when moving between pages
-export * from "./routes";
+// pathnames to be used when moving between pages
+export * from "./pathnames";
