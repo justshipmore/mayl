b93b3d3331070afbd34a97915ef52734a6a78bac
justshipmore <justshipmore@gmail.com>
2024-05-17 20:15:28 +0100

    Add addition route paths to utility package

--- packages/utils/src/constants/pathnames.ts
+++ packages/utils/src/constants/pathnames.ts
@@ -1,3 +1,11 @@
+// marketing routes
+export const LANDING_PAGE_PATH = '/'
+
+// app routes
 export const APP_WORKSPACE_PATH = '/app.mayl.co';
 export const APP_SELECT_TEMPLATE_PATH = '/app.mayl.co/templates';
 export const APP_EDITOR_PATH = '/app.mayl.co/templates/editor'
+
+// auth routes
+export const AUTH_LOGIN_PATH = '/app.mayl.co/login'
+export const AUTH_REGISTER_PATH = '/app.mayl.co/register'
