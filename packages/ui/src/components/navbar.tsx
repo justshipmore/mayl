22058c03bc40aa173f2ae2b5617dc809fe57d320
justshipmore <justshipmore@gmail.com>
2024-05-17 20:14:22 +0100

    Add shadcn components

---
+++ packages/ui/src/components/navbar.tsx
@@ -0,0 +1,66 @@
+import { AUTH_LOGIN_PATH, AUTH_REGISTER_PATH } from "@repo/utils/constants";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const navItems = [
+  {
+    name: "Overview",
+    slug: "#",
+  },
+  {
+    name: "Features",
+    slug: "#",
+  },
+  {
+    name: "Prices",
+    slug: "#",
+  },
+];
+
+function Navbar() {
+  return (
+    <div className="bg-background sticky top-0 flex border-b">
+      <header className="container mx-auto flex h-16  items-center gap-4 px-4 md:px-6">
+        <nav className="hidden flex-grow flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="25"
+            height="40"
+            fill="none"
+            viewBox="0 0 40 40"
+          >
+            <path
+              fill="#000"
+              d="M20 0c11.046 0 20 8.954 20 20v14a6 6 0 0 1-6 6H21v-8.774c0-2.002.122-4.076 1.172-5.78a10 10 0 0 1 6.904-4.627l.383-.062a.8.8 0 0 0 0-1.514l-.383-.062a10 10 0 0 1-8.257-8.257l-.062-.383a.8.8 0 0 0-1.514 0l-.062.383a9.999 9.999 0 0 1-4.627 6.904C12.85 18.878 10.776 19 8.774 19H.024C.547 8.419 9.29 0 20 0Z"
+            ></path>
+            <path
+              fill="#000"
+              d="M0 21h8.774c2.002 0 4.076.122 5.78 1.172a10.02 10.02 0 0 1 3.274 3.274C18.878 27.15 19 29.224 19 31.226V40H6a6 6 0 0 1-6-6V21ZM40 2a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"
+            ></path>
+          </svg>
+
+          {navItems.map((item) => (
+            <Link
+              href={item.slug}
+              className="text-muted-foreground hover:text-foreground transition-colors"
+            >
+              {item.name}
+            </Link>
+          ))}
+        </nav>
+        <span className="flex gap-4">
+          <Link href={AUTH_LOGIN_PATH}>
+            <Button size="sm" variant="secondary">
+              Login
+            </Button>
+          </Link>
+          <Link href={AUTH_REGISTER_PATH}>
+            <Button size="sm">Sign up</Button>
+          </Link>
+        </span>
+      </header>
+    </div>
+  );
+}
+
+export { Navbar };
---
+++ packages/ui/src/index.ts
@@ -0,0 +1,8 @@
+// component library
+export * from "./components/ui/button";
+export * from "./components/ui/card";
+export * from "./components/ui/label";
+export * from "./components/ui/input";
+
+// custom components
+export * from "./components/navbar"; 
