9106b656cecd7e200c215a3e3cf13cb6c8e5dde9
justshipmore <justshipmore@gmail.com>
2024-05-18 21:35:16 +0100

    Add more shadcn components for workspaces

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -3,6 +3,8 @@ export * from "./components/ui/button";
 export * from "./components/ui/card";
 export * from "./components/ui/label";
 export * from "./components/ui/input";
+export * from "./components/ui/dropdown-menu";
+export * from "./components/ui/avatar";
 
 // custom components
 export * from "./components/navbar"; 
